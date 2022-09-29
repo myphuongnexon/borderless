82a66973b9b62a80d69d3d39797bcab862109c90
myphuong <lyphuong@nexondv.com>
2022-09-29 16:14:33 +0700

    fix i18n

--- src/global.ts
+++ src/global.ts
@@ -1,6 +1,6 @@
 import { i18n } from "./i18n/i18n";
 export const mode = localStorage.getItem('mode');
-export const language = JSON.parse(window.localStorage.language) ;
+export const language = window.localStorage.language ? JSON.parse(window.localStorage.language) : 'en' ;
 
 export const changeLanguage = (values : any) => {
     i18n.changeLanguage(values);
